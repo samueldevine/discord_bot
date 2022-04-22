d95bd392a43cfabd6bfe4c144b69e96198befa28
Sam Devine <samuel.will.devine@gmail.com>
2022-04-21 23:30:26 -0600

    idk

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 // Run dotenv
 require('dotenv').config();
+// import fetch from "node-fetch";
 
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
@@ -10,8 +11,24 @@ client.on('ready', () => {
 
 client.login(process.env.DISCORD_TOKEN);
 
+// function get_summoner_data(summoner_name) {
+//     fetch(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summoner_name}`)
+//         .then(response => {
+//             return response.json();
+//         })
+//         .then(users => {
+//             console.log(users);
+//         });
+// };
+
 client.on('messageCreate', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('pong');
+    switch (msg.content) {
+        case 'ping':
+            msg.reply('pong');
+            break;
+        // case 'bwolf77':
+        //     response = get_summoner_data('bwolf77');
+        //     msg.reply(response);
+        //     break;
     }
 });
